cbb2167210761aa0642f69fbcc814e80564acda8
csvernon <csvernon@ncsu.edu>
2019-05-18 01:12:37 -0400

    please work

--- public/controllers/burgersController.js
+++ public/controllers/burgersController.js
@@ -3,41 +3,38 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
-router.get("/", function (req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
-  
-  router.get("/api/burgers", function (req, res) {
-    db.burgers.findAll({})
-      .then(function (burgerData) {
-        res.json(burgerData);
-      });
-  });
+
+router.get("/api/burgers", function (req, res) {
+  db.burgers.findAll({})
+    .then(function (burgerData) {
+      res.json(burgerData);
+    });
+});
 
 // post route
 router.post("/api/burgers", function (req, res) {
-    console.log("Req.body" + req.body);
-    db.burgers.create({
-      burger_name: req.body.burger_name,
-      devoured: false
-    })
-      .then(function (burgerData) {
-        res.json(burgerData);
-      });
-  });
+  console.log("Req.body" + req.body);
+  db.burgers.create({
+    burger_name: req.body.burger_name,
+    devoured: false
+  })
+    .then(function (burgerData) {
+      res.json(burgerData);
+    });
+});
 
 // put route
 router.put("/api/burgers/:id", function (req, res) {
-    db.burgers.update({
-      devoured: true
-    }, {
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(function (burgerData) {
-        res.json(burgerData);
-      });
-  });
-  
-  module.exports = router;
+  db.burgers.update({
+    devoured: true
+  }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function (burgerData) {
+      res.json(burgerData);
+    });
+});
+
+module.exports = router;
